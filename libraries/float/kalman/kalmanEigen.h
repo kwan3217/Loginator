c68778edc01e38ab703b7e972fcf8afb70b1e7b5
Chris Jeppesen (Omoikane) <omoikane.kwansystems.org>
2016-01-17 22:36:41 -0700

    Implemented Kalman Velocity

--- libraries/float/kalman/kalmanEigen.h
+++ libraries/float/kalman/kalmanEigen.h
@@ -11,7 +11,30 @@ the matrix inverse involved in calculating the Kalman gain is turned into a
 scalar divide. We call this element-by-element operation the scalar aspect.
 
 This is implemented according to the notation on the Kwan Hypertext Library
-Kalman filter page, https://omoikane.kwansystems.org/wiki/index.php/Kalman_filter
+Kalman filter page, https://omoikane.kwansystems.org/wiki/index.php/Kalman_filter .
+
+The Kalman filter is an algorithm which uses a model of how a physical system
+works, combined with a series of observations of the system, to produce a series
+of estimates of the state of the system. The filter works in the presence of
+measurement and model uncertainty, and in fact is designed to give the best
+possible estimate of the state, as well as an estimate of the uncertainty of the
+estimate.
+
+The simplest form of the filter is the linear filter. In this case, the physics
+of the system are all perfectly described with matrices, as follows:
+
+$$\vec{x}_i=\M{A}\vec{x}_{i-1}+\vec{w}_{i}$$
+$$\vec{z}_i=\M{H}\vec{x}_{i}+\vec{v}_{i}$$
+
+where
+
+*  * $i$ is the measurement index
+*  * $\vec{x}_i$ is the (unknown) actual state of the system at measurement $i$. This is an $n_{state}$-dimensional vector, or one with $n_{state}$ components.
+*  * $\M{A}$ is the state transition matrix. Given a previous state, the state transition matrix is used to transform it to the current state. Since it operates on an $n$-dimensional vector and returns an $n$-dimensional vector (the number of components in the state never changes) it must be an $n\times n$ square matrix. This matrix might change between measurements, but the filter presumes that it is constant.
+*  * $\vec{w}_i$ is the (unknown) process noise, an $n_{state}$-dimensional Gaussian random vector with a zero mean and covariance matrix $\M{Q}$. This is uncertainty in the process itself, unrelated to measurement. It implies that the next state is not perfectly determined by the previous state. Since the process noise is compatible with addition to the state vector, it must be a vector with $n$ components, and its covariance must be a matrix with $n\times n$ components.
+*  * $\vec{z}_i$ is the measurement vector for measurement $i$. This is an $m$-dimensional vector, where $m$ may be greater than, less than, or equal to $n$.
+*  * $\M{H}$ is the observation matrix. Given a state, the observation matrix is used to transform it to an observation vector. Since it operates on an $n$-dimensional vector and returns an $m$-dimensional vector (the number of components of the observation may be different than the number of components of the state) it must be an $m \times n$ matrix. This matrix might change between measurements, but the filter presumes that it is constant.
+*  * $\vec{v}_i$ is the (unknown) measurement noise, an $m$-dimensional Gaussian random vector with a zero mean and covariance matrix $\M{R}$. Since the measurement noise is compatible with addition to the measurement vector, it must be a vector with $m$ components, and its covariance must be a matrix with $m \times m$ components.
 
 By itself, this implements a linear Kalman filter. The A, H, Q, and R matrices
 have to be initialized outside of this routine.
@@ -73,7 +96,7 @@ public:
       Uses CRTP. Doesn't do anything in linear filter, nonlinear filters will
       probably implement a matrix numerical integrator here */
   /*virtual*/ void fA() {};
-  /** Measurement matrix. Given x, calculates the jacobian of the measurement
+  /** Measurement matrix. Given x, calculates the Jacobian of the measurement
       function with respect to the state vector at the given value of the
       state vector. Doesn't do anything in linear filter, the external code will
       have to set H directly. Implemented in derived class.
@@ -83,15 +106,14 @@ public:
       measurement value */
   /*virtual*/ void fg() {zh=H*xh;};
 
-  //fPhi-
-  //fH  -
-  //Take a step using a scalar observation
-  //z  - scalar measurement
-  //dt_major - time step size
-  //R   - Measurement covariance. Scalar since the measurement is scalar
-  void step(Scalar dt, Eigen::Matrix<Scalar,n_meas,1>& z);
+  /*virtual*/ void timeUpdate(Scalar dt_i);
+  /*virtual*/ void stateUpdate() {xh=A*xh;};
+  void measUpdate(Eigen::Matrix<Scalar,n_meas,1>& z);
+  void step(Eigen::Matrix<Scalar,n_meas,1>& z, Scalar dt_i=0) {if(dt_i>0) static_cast<T*>(this)->timeUpdate(dt_i); measUpdate(z);};
 };
-//virtual void fH()=0;   //
+
+template<int n_state, int n_meas, typename Scalar=fp>
+class LinearKalman:public Kalman<n_state, n_meas, LinearKalman<n_state,n_meas,Scalar>,Scalar> {};
 
 #include "kalmanEigen.inc"
 
--- matrixTest/main.cpp
+++ matrixTest/main.cpp
@@ -4,6 +4,9 @@
 #include "kalmanEigen.h"
 #include <random>
 
+//These are a "scarlet letter". If you need these to compile, it means that Eigen
+//is trying to print a message, and your code could (almost certainly will) fail.
+#ifdef SCARLET_LETTER
 extern "C" {
 void __attribute__ ((weak)) _exit(int status) {};
 void __attribute__ ((weak)) _sbrk() {};
@@ -16,20 +19,15 @@ void __attribute__ ((weak)) _isatty() {};
 void __attribute__ ((weak)) _lseek() {};
 void __attribute__ ((weak)) _read() {};
 }
-
-class KalmanVelocity: public Kalman<2,1,KalmanVelocity> {
-
-};
-
-class KalmanConstant: public Kalman<1,1,KalmanConstant> {
-
-};
+#endif
 
 uint32_t ticks;
 fp t;
-const fp dt=0.01;
-KalmanConstant kc;
+const fp tps=100;
+const fp dt=1.0/tps;
+LinearKalman<2,1,fp> kc;
 const fp sig_z=0.1;
+const fp sig_v=0.1;
 std::default_random_engine generator;
 std::normal_distribution<fp> distribution(0.0,sig_z);
 Eigen::Matrix<fp,1,1> z;
@@ -38,17 +36,41 @@ void setup() {
   pinMode(13,OUTPUT);
   Serial.begin(4800);
   Serial.println("Begin");
-  kc.A << 1;
-  kc.H << 1;
-  kc.Q << 0;
+  kc.A << 1, dt,
+		  0,  1;
+  kc.H << 1,  0;
+  kc.Q << dt*dt*dt*dt/4,dt*dt*dt   /2,
+		  dt*dt*dt   /2,dt*dt        ;
   kc.R << sig_z*sig_z;
-  kc.P << 1;
-  kc.xh << 0;
+  kc.P << 1,0,
+		  0,1;
+  kc.xh << 0,
+		   0;
+}
+
+fp flare(int tick) {
+  const fp flareStartTime=5;
+  fp t=fp(tick)/tps;
+  if(t<flareStartTime) return 20;
+  const fp flarePeakDt=1; //Number of seconds from start to peak
+  const fp flareA=log(2)/flarePeakDt;
+  return 20+20*4*(exp(-flareA*(t-flareStartTime))-exp(-2*flareA*(t-flareStartTime)));
 }
 
 void loop() {
-  z << 0.5+distribution(generator);
-  kc.step(dt,z);
-  Serial.print("xh[0]: ");
-  Serial.println(kc.xh(0),6);
+  fp True=flare(ticks);
+  z << True+distribution(generator);
+  kc.step(z,dt);
+  Serial.print("t: ");
+  Serial.print(fp(ticks)/tps,6);
+  Serial.print("true: ");
+  Serial.print(True,6);
+  Serial.print("meas: ");
+  Serial.print(z(0),6);
+  Serial.print("  xh[0]: ");
+  Serial.print(kc.xh(0),6);
+  Serial.print("  xh[1]: ");
+  Serial.println(kc.xh(1),6);
+  ticks++;
+  if(ticks==1000) blinklock(1000);
 }
