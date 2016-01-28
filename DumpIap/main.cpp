76ffd8b9f28b8262640971cb597ada72be86c873
Chris Jeppesen <chrisj@mio.kwansystems.org>
2016-01-28 15:46:58 -0700

    More notes, added code to do the disassembly to the Makefile

--- DumpIap/main.cpp
+++ DumpIap/main.cpp
@@ -3,11 +3,15 @@
 #include "LPCduino.h"
 #include "dump.h"
 
+int __attribute__((__target__("thumb"))) thumb_fn(int a) {
+  return a*2;
+}
+
 void setup() {
   Serial.begin(38400);
   setup_clock();
   IntelHex dump(Serial);
-  dump.region((char*)0x7D000,0x7D000,12*1024,32);
+  dump.region((char*)0x7D000,0x7D000,12*1024,thumb_fn(16));
 }
 
 void loop() {
