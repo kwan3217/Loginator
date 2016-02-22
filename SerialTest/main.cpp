aded5e112c11bc6978f51aee038500253e0462cd
Chris Jeppesen <chrisj@mio.kwansystems.org>
2016-02-22 16:05:39 -0700

    More bootstrap hacking

--- SerialTest/main.cpp
+++ SerialTest/main.cpp
@@ -52,6 +52,13 @@ void printreg(unsigned int i) {
   printreg(i,i);
 }
 
+volatile unsigned int& IOCON_P0_0=*((unsigned int*)0x4002'c000);
+volatile unsigned int& DIR0 =*((unsigned int*)0x2009'8000);
+volatile unsigned int& PIN0 =*((unsigned int*)0x2009'8014);
+volatile unsigned int& SET0 =*((unsigned int*)0x2009'8018);
+volatile unsigned int& CLR0 =*((unsigned int*)0x2009'8018);
+volatile unsigned int& MASK0=*((unsigned int*)0x2009'8010);
+
 void setup() {
   //Turn on FPU
   volatile unsigned int& CPACR=*((unsigned int*)0xE000'ED88);
@@ -59,30 +66,31 @@ void setup() {
   //Map our vector table into place
   volatile unsigned int& MEMMAP=*((unsigned int*)0x400F'C040);
   MEMMAP=1;
-  
-  Serial.begin(38400);
-  printreg(0x0010043c);
 
-  //There appears to be a block of registers at around 0x0010'0000 and a lock bit 
-  //for it at 0x0020'0000, perhaps in turn with a lock bit for it at 0x0x0020'0010. Experiment with these bits.
-  volatile unsigned int& lockHi=*((unsigned int*)0x0020'0010);
-  volatile unsigned int& lockLo=*((unsigned int*)0x0020'0000);
-  lockHi=7;
-  lockLo=0x145;
-  for(int i=0;i<15;i++) CPACR=CPACR;
-  lockLo|=(1<<6);
-  for(int i=0;i<15;i++) CPACR=CPACR;
-  printreg(0x0010043c);
-  lockLo&=~(1<<6);
-  for(int i=0;i<15;i++) CPACR=CPACR;
-  printreg(0x0010043c);
+  //Turn on GPIO (directly with registers to start, then adapt libraries)
+  PCONP()|=(1<<15); //Turn on GPIO subsystem (should already be on)
+  IOCON_P0_0=(0<< 0) |  //Function 0, GPIO
+             (0<< 3) |  //No pullup/pulldown
+             (0<< 5) |  //No hysteresis
+             (0<< 6) |  //Not inverted
+             (0<< 9) |  //Output slew enabled
+             (0<<10) ;  //Normal push-pull mode (not simulated open-drain)
+  DIR0=(1<<0); //Set pin 0 to output
+  SET0=(1<<0); //Set pin to high
+  MASK0=0; //Enable all bits 
+  Serial.begin(38400);
 }
 
 typedef void (*fvoid)(void);
 
 void loop() {
-  volatile float f=2.0;
-  f=sqrtf(f*2.7f);
+  static int i=0;
+  fp f=sqrtf(2.0f*2.7f);
   Serial.println(f,6); 
-  for(;;); //Don't run any farther
+  if (i==1) {
+    SET0=1;
+  } else {
+    CLR0=1;
+  }
+  i=1-i;
 }
